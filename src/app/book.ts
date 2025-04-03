1b686c3975e01797bfc28961a20900513fd16043
LorenzoPuppo95 <puppolorenzo@live.it>
2025-04-03 08:33:51 +0200

    fix images

--- src/app/book.ts
+++ src/app/book.ts
@@ -1,8 +1,23 @@
+// export interface Book {
+//     id: number;
+//     title: string;
+//     authors: {}[];
+//     summaries: string[];
+//     subjects: string[];
+//     imagejpeg: string
+//   }
+
 export interface Book {
-    id: number;
-    title: string;
-    authors: {}[];
-    summaries: string[];
-    subjects: string[];
-    imagejpeg: string
-  }
+  id: number
+  title: string
+  authors: Author[]
+  summaries: string[]
+  subjects: string[]
+  imagejpeg: string
+}
+
+export interface Author {
+  name: string
+  birth_year: any
+  death_year: any
+}
